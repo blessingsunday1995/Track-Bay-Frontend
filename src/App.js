54c9cbf54d18e5fa58624d6b6633e7e77553b5a7
Drpampam <johnomotoye@gmail.com>
2023-01-18 23:29:37 +0100

    removed link from form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import CrmDivComponent from './component/CrmDivComponent'
 import CrmSection from './component/CrmSection';
+import OurProduct from './component/OurProduct';
+import ContactUs from './component/ContactUs';
 
 function App() {
   return (
@@ -8,6 +10,10 @@ function App() {
       <CrmDivComponent />
       <div className="content">
         <CrmSection />
+        <div className="">
+          <OurProduct />
+          <ContactUs />
+        </div>
       </div>
     </div>
   );
