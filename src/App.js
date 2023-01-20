3afd54d96ce760fe19533014a2f660a78f085bf3
Olu-nife <maryiyanuoluwa2015@gmail.com>
2023-01-20 15:04:00 -0800

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import './App.css';
+import './components/Home';
+import './components/Header/Home';
+import Navbar from "./components/Nav/Nav";
+import Features from "./components/Features";
+import Pricing from "./components/Pricing";
+import Webinars from "./components/Webinars";
+import Blog from "./components/Blog";
+import Pages from "./components/Pages";
+import Newsletter from "./components/Newsletter";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Header from './components/Header/Home';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello! Welcome to CX- 365</h1>
-    </div>
+    <Router>
+      <div className='App'>
+        <ul>
+          <li><Link to="/features" Features></Link></li>
+          <li><Link to="/pricing" Pricing></Link></li>
+          <li><Link to="/webinars" Webinars></Link></li>
+          <li><Link to="/blog" Blog></Link></li>
+          <li><Link to="/pages" Pages></Link></li>
+          <li><Link to="/newsletter" Newsletter></Link></li>
+        </ul>
+      </div>
+      <Header />
+      <Navbar />
+      <Routes>
+
+        <Route exact path='/features' element={<Features />}></Route>
+        <Route exact path='/pricing' element={<Pricing />}></Route>
+        <Route exact path='/webinars' element={<Webinars />}></Route>
+        <Route exact path='/blog' element={<Blog />}></Route>
+        <Route exact path='/pages' element={<Pages />}></Route>
+        <Route exact path='/newsletter' element={<Newsletter />}></Route>
+      </Routes>
+    </Router>
+
   );
 }
 
